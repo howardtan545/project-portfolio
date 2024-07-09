5effd8c3dec9d63cc4a5b00e1bc811d75bee2672
Howard <howardtan545@gmail.com>
2024-07-08 20:48:53 -0400

    Added Header Page

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,16 @@
+function Header(){
+
+    return(
+        <header>
+            <h1>Project Portfolio</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
